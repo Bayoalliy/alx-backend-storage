396df5ddb961f62a08e29488433567118b72d8ee
Bayoalliy <adebayoalliy@gmail.com>
2025-01-08 13:33:13 -0800

    update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,5 +6,6 @@ school document based on the name
 
 
 def update_topics(mongo_collection, name, topics):
+    """updates mongo_collection"""
     mongo_collection.update_many(
             {'name': name}, {'$set': {'topics': topics}})
